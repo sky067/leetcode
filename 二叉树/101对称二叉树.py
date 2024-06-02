f0c6aad817acede5330fd058c99381286d9ea41c
w <w@w.com>
2024-06-03 00:50:04 +0800

    binary tree

---
+++ 二叉树/101对称二叉树.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @File    : 101对称二叉树.py
+# @Des     :
+
+"""
+相等二叉树的变种
+难度：simple
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        def is_sample_tree(left, right):
+            # 收否都为空
+            if left is None or right is None:
+                return left is right
+            # 判断节点是否相等
+            if left.val != right.val:
+                return False
+            if not (is_sample_tree(left.left, right.right) and is_sample_tree(left.right, right.left)):
+                return False
+            return True
+
+        return is_sample_tree(root.left, root.right)
